d6c6ed852b6edbf03b68acd1c857ae8fcae670e9
Matheus Haruo <haruo.fiap@gmail.com>
2025-06-15 22:37:42 -0300

    Updates Finais

--- backend/ml.py
+++ backend/ml.py
@@ -3,7 +3,6 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 import numpy as np
 
-# Carrega dados
 bets = pd.read_json('bets.json')
 dados = pd.read_json('logs/registros.json')
 teste = dados.tail(1)  
@@ -11,16 +10,13 @@ teste = dados.tail(1)
 X = bets.drop(['aposta_fast'], axis=1)
 y = bets['aposta_fast']
 
-# Log na aposta
 X['valor_aposta'] = np.log1p(X['valor_aposta'])
 teste['valor_aposta'] = np.log1p(teste['valor_aposta'])
 
-# Dummies
 x_dummies = pd.get_dummies(X, columns=['time'], dtype=int)
 teste = pd.get_dummies(teste, columns=['time'], dtype=int)
 teste = teste.reindex(columns=x_dummies.columns, fill_value=0)
 
-# Treino e predição
 X_train, _, y_train, _ = train_test_split(x_dummies, y, test_size=0.2, random_state=42, stratify=y)
 clf = RandomForestClassifier(n_estimators=100, class_weight='balanced', random_state=42)
 clf.fit(X_train, y_train)
